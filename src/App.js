36fbc5a6187b0a854a385a93a74c8752bbcc603c
chominho14 <chominho14@naver.com>
2022-01-24 20:43:48 +0900

    React useEffect -1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { useState, memo } from "react";
+import React, { useState, memo, useEffect } from "react";
 import PropTypes from "prop-types";
 import Button from "./Button";
 import styles from "./App.module.css";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue(prev => prev + 1);
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
       <Button text={"Continue"} />
     </div>
   );
