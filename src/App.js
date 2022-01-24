f39d3326decdd963c732364f70562c50c5350f17
chominho14 <chominho14@naver.com>
2022-01-24 20:24:52 +0900

    React Styles CSS

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 import React, { useState, memo } from "react";
 import PropTypes from "prop-types";
-
-function Btn({ text, onClick, fontSize = 16 }) {
-  console.log(text, "was render");
-  return (
-    <button
-      onClick={onClick}
-      style={{
-        backgroundColor: "tomato",
-        color: "white",
-        padding: "10px 20px",
-        borderRadius: 10,
-        border: 0,
-        fontSize,
-      }}
-    >
-      {text}
-    </button>
-  );
-}
-
-Btn.propTypes = {
-  text: PropTypes.string.isRequired,
-  fontSize: PropTypes.number.isRequired,
-};
-
-const MemorizedBtn = memo(Btn);
+import Button from "./Button";
+import styles from "./App.module.css";
 
 function App() {
-  const [value, setValue] = useState("Save Changes");
-  const changeValue = () => {
-    setValue("Revert Changes");
-  };
   return (
     <div>
-      <MemorizedBtn text={value} onClick={changeValue} />
-      <MemorizedBtn text="Save Changes" fontSize={18} />
+      <h1 className={styles.title}>Welcome back!</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
